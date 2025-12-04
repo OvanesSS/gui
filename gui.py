c65948d2115c0853c90d4fbc6a4f37f9a02c73af
work <ovanes12@icloud.com>
2025-12-04 14:35:57 +0300

    04.12.25 Update interface and back functions

--- gui.py
+++ gui.py
@@ -1,20 +1,29 @@
 import customtkinter as ctk
 import tkinter as tk
 from serial.tools import list_ports as tools
-import back
-import queue
+import myclassport
 
-def test():
-    print(combo_vars[1][1].get(), combo_vars[2][1].get(), combo_vars[3][1].get(), combo_vars[4][1].get(), combo_vars[5][1].get())
+port=None
+
+
+def button_connect():
+    global port
+    if condition.get() == 'Connect':
+        condition.set('Disconnect')
+        port = myclassport.ComPortProcessor(combo_vars[1][1].get(), combo_vars[2][1].get(), combo_vars[3][1].get(),
+                                            combo_vars[4][1].get(), combo_vars[5][1].get())
+        port.start_thread()
+    else:
+        condition.set('Connect')
+        port.close_port()
 
 labels_parametrs = {1:'Port:', 2:'Speed:', 3:'Bits:', 4:'Stop bits:', 5:'Parity:'}
 combo_values={1:[info.device for info in tools.comports()],
-              2:['1200', '2400', '4800', '9600', '14400', '19200', '28800', '38400'],
-              3:['7', '8'],
+              2:['9600', '1200', '2400', '4800', '14400', '19200', '28800', '38400'],
+              3:['8', '7'],
               4:['1', '2'],
               5:['None', 'Even', 'Odd', 'Mark', 'Space']}
 combo_vars = {}
-data_queue = queue.Queue()
 
 
 def set_window_location(win, win_width, win_height):
@@ -45,39 +54,61 @@ ctk.set_default_color_theme('blue')
 app = ctk.CTk()
 app.title('KF1')
 app.resizable(False, False)
-set_window_location(app, 650, 505)
+set_window_location(app, 698, 680)
 
 for (key, value) in combo_values.items():
     combo_vars[key] = [value, tk.StringVar(value = value[0])]
+condition = tk.StringVar(value = 'Connect')
 
-
-frame_parametrs = ctk.CTkFrame(master=app)
-frame_parametrs.grid(row=0, column = 0, pady=10, padx=10, sticky='ew')
-ctk.CTkLabel(master=frame_parametrs, text = 'Parametrs').grid(row=0, column=0, columnspan = 2, ipady = 5)
+frame_config = ctk.CTkFrame(master=app)
+frame_config.grid(row=0, column=0, padx=[5,0], pady=[5,0], sticky='n')
+frame_parametrs = ctk.CTkFrame(master=frame_config)
+frame_parametrs.grid(row=0, column=0)
+ctk.CTkLabel(master=frame_parametrs, text = 'Parametrs').grid(row=0, column=0, columnspan=2)
 for (key, value) in labels_parametrs.items():
-    ctk.CTkLabel(master=frame_parametrs, text = value).grid(row=key, column=0, sticky = 'w', padx=10)
-
+    ctk.CTkLabel(master=frame_parametrs, text = value).grid(row=key, column=0, padx=5, sticky='w')
 for (key, value) in combo_vars.items():
     ctk.CTkComboBox(master=frame_parametrs, values=value[0], variable=value[1], height=25, width=100,
-                    justify='center', state='readonly').grid(row=key, column=1, sticky='e', padx=10)
-connect = ctk.CTkButton(master=frame_parametrs, text='Connect', command=test)
-connect.grid(row=6, column=0, columnspan = 2, pady=10, padx = 25)
+                    justify='center', state='readonly').grid(row=key, column=1, padx=5)
+connect = ctk.CTkButton(master=frame_parametrs, textvariable = condition, command=button_connect)
+connect.grid(row=6, column=0, columnspan=2, pady=5)
 
-frame_commands = ctk.CTkFrame(master=app)
-frame_commands.grid(row=1, column = 0, sticky='ew', pady=10, padx=10)
-ctk.CTkLabel(master=frame_commands, text = 'Commands').grid(row=0, column=0, ipady=5, sticky='ew')
-mcal = ctk.CTkButton(master = frame_commands, text='MCAL')
-mcal.grid(row=1, column=0,padx=25)
-setfil = ctk.CTkButton(master = frame_commands, text='SETFIL', command = open_setfil)
-setfil.grid(row=2, column=0,padx=25, pady=10)
+frame_commands = ctk.CTkFrame(master=frame_config)
+frame_commands.grid(row=1, column=0, pady=[5,0])
+ctk.CTkLabel(master=frame_commands, text='Commands').grid(row=0, column=0,)
+mcal = ctk.CTkButton(master=frame_commands, text='MCAL')
+mcal.grid(row=1, column=0, padx=16, pady=[0,5])
+setfil = ctk.CTkButton(master=frame_commands, text='SETFIL', command=open_setfil)
+setfil.grid(row=2, column=0, pady=[0,5])
+
 
 frame_answers = ctk.CTkFrame(master=app)
-frame_answers.grid(row=0,column = 1, rowspan=10, padx=10, pady=10, sticky = 'nsew', ipady=5)
-ctk.CTkLabel(master=frame_answers, text = 'Answers').grid(row=0, column=0, sticky = 'n', pady=5)
-all = ctk.CTkTextbox(master=frame_answers, height=200, width = 400, activate_scrollbars=True)
-all.grid(row=1, column=0, sticky = 'n',padx=10)
-ctk.CTkLabel(master=frame_answers, text = 'Responses to commands').grid(row=2, column=0, sticky = 'n', pady=5)
-responses = ctk.CTkTextbox(master=frame_answers, height=200, width = 400, activate_scrollbars=True)
-responses.grid(row=3, column=0, sticky = 'n',padx=10)
+frame_answers.grid(row=0, column=1, padx=5, pady=5)
+ctk.CTkLabel(master=frame_answers, text = 'Answers').grid(row=0, column=0)
+all_ans = ctk.CTkTextbox(master=frame_answers, activate_scrollbars=True, width=500, height=300,)
+all_ans.grid(row=1, column=0, padx=5, pady=[0,10])
+ctk.CTkLabel(master=frame_answers, text = 'Responses to commands').grid(row=2, column=0)
+responses = ctk.CTkTextbox(master=frame_answers, activate_scrollbars=True, width=500, height=300,)
+responses.grid(row=3, column=0, padx=5, pady=[0,5])
+
+
+def update_textbox():
+    while not port.queue.empty():
+        all_ans.insert(0.0, ''.join(hex(ch) for ch in port.queue.get(timeout=1)) + '\n')
+def timer_textbox():
+    if port is not None:
+        update_textbox()
+    app.after(1000, timer_textbox)
+timer_textbox()
+
+
+def window_exit():
+    if port is not None:
+        port.close_port()
+    app.destroy()
+app.protocol("WM_DELETE_WINDOW", window_exit)
+
+
 app.mainloop()
+
 
---
+++ myclassport.py
@@ -0,0 +1,34 @@
+import threading
+import serial
+import queue
+
+parity_values = {'None':serial.PARITY_NONE, 'Even':serial.PARITY_EVEN, 'Odd':serial.PARITY_ODD, 'Mark':serial.PARITY_MARK,
+                'Space':serial.PARITY_SPACE}
+
+class ComPortProcessor:
+    def __init__(self, port, baudrate, bits, sbits, parity):
+        self.port = serial.Serial(port=port, baudrate=baudrate, bytesize=int(bits), stopbits=int(sbits), parity=parity_values[parity],
+                  timeout=1)
+        self.event = threading.Event()
+        self.queue = queue.Queue()
+
+    def read_in_thread(self):
+        n = 0
+        while True:
+            if self.event.is_set():
+                self.port.close()
+                self.event.clear()
+                break
+            else:
+                n+=1
+                data = self.port.read(24)
+                if data != b'':
+                    self.queue.put(data)
+                    print(f'Из потока порта ', data, n)
+
+    def close_port(self):
+        self.event.set()
+
+    def start_thread(self):
+        thread_port = threading.Thread(target=self.read_in_thread,)
+        thread_port.start()
